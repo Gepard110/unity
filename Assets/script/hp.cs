a66f2a316d588a44a72898615c8c6de42c6feacc
BuildTools <hiro-spica830@i.softbank.jp>
2015-08-30 20:19:28 +0900

    あ

---
+++ Assets/script/hp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class hp : MonoBehaviour {
+	public int life = 120;
+	public Text a; 
+	public bool flg = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (life < 1) {
+			Debug.Log ("DIED");
+		}
+	}
+
+	public void test(){
+		if (flg == true) {
+			flg = false;
+			life -= 1;
+			a.text = life.ToString ();
+			StartCoroutine ("hogehoge");
+		}
+	}
+		IEnumerator hogehoge(){
+			yield return new WaitForSeconds(2);
+			flg = true;
+		}
+	}
--- Assets/script/shot.cs
+++ Assets/script/shot.cs
@@ -1,18 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class shot : MonoBehaviour
-{
+using UnityEngine.UI;
+public class shot : MonoBehaviour {
+	public int i = 200;
+	public bool flag = true;
+	public Text a;
+	public bool flg = false;
+	public bool fl = false;
 	// Use this for initialization
-	void Start ()
-	{
+	void Start () {
+
 	}
 	
 	// Update is called once per frame
-	void Update ()
-	{
-		if (Input.GetKeyDown ("1")) {
-			GetComponentInChildren<ParticleSystem> ().Play ();
-		}
+	void Update (){ 
+		if (flg) s ();
+		if (fl) r ();
 	}
-}
+	public void s(){
+		if (flag == true) {
+			//if (Input.GetKey ("1")) {
+				GetComponentInChildren<ParticleSystem> ().Play ();
+				i --;
+				a.text = i.ToString ();
+			//}
+		}
+			if (i < 1) {
+				flag = false; 
+				StartCoroutine ("hogehoge");
+			}
+		}
+		IEnumerator hogehoge(){
+		yield return new WaitForSeconds(1);
+				flag = true;
+				i =  200;
+				a.text = i.ToString();
+			}
+	public void r(){
+		i = 200;
+		a.text = i.ToString ();
+	}
+	public void change(){
+		flg = !flg;
+	}
+	public void chan(){
+		fl = !fl;
+	}
+		}
+
